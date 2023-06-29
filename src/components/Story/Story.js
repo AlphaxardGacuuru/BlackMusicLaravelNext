60bd7426e85dd1b870e9966c14331a32e564c537
Alphaxard Gacuuru <alphaxardgacuuru47@gmail.com>
2023-06-29 14:55:17 +0300

    Moved progress bar to seperate component

--- src/components/Story/Story.js
+++ src/components/Story/Story.js
@@ -5,126 +5,15 @@ import axios from "@/lib/axios"
 import Img from "next/image"
 
 import CloseSVG from "@/svgs/CloseSVG"
+import StoryProgressBar from "./StoryProgressBar"
 
 const Story = (props) => {
 	const [timer, setTimer] = useState()
-	const [percent, setPercent] = useState("0%")
 	const [sendSeenAt, setSendSeenAt] = useState()
 	const [hasMuted, setHasMuted] = useState(props.story.hasMuted)
 	const [segment, setSegment] = useState(0)
 	const [freeze, setFreeze] = useState(true)
-
-	/*
-	 * Intersection Observer API
-	 */
-
-	var t
-	var time = 0
-	var index = 0
-
-	// Set options
-	let options = {
-		root: null,
-		rootMargin: "0px",
-		threshold: 0.9,
-	}
-
-	useEffect(() => {
-		setTimeout(() => {
-			let observer = new IntersectionObserver(callback, options)
-
-			const storyEl = document.getElementById(props.story.id)
-
-			observer.observe(storyEl)
-		}, 500)
-
-		return () => clearInterval(t)
-	}, [])
-
-	/*
-	 * Intersection Observer API Callback function */
-	let callback = (entries, observer) => {
-		if (entries != "undefined") {
-			entries.forEach((entry) => {
-				// Start or Clear progress bar
-				if (entry.isIntersecting) {
-					time = 0
-					setSegment(0)
-					handleTimer()
-				} else {
-					// Use t instead of timer to prevent rerendering
-					clearInterval(t)
-					setPercent("0%")
-				}
-			})
-		}
-	}
-
-	/*
-	 * Handle Timer */
-	const handleTimer = () => {
-		t = setInterval(() => {
-			// Stop incrementing after 5s (50 because its deciseconds)
-			if (time < 55) {
-				handleProgressBar()
-			} else {
-				// Go to next story segment
-				const length = props.story.media.length
-				const length2 = length - 1
-
-				if (segment < length2 && length > 1) {
-					setSegment(segment + 1)
-					time = 0
-				} else {
-					scrollToNextStory()
-				}
-			}
-		}, 100)
-
-		setTimer(t)
-	}
-
-	/*
-	 * Function for incrementing progress bar */
-	const handleProgressBar = () => {
-		// Increment time by 1
-		time++
-		// Get percentage
-		var per = (time * 100) / 50
-		// Format percentage for style
-		per = per + "%"
-		// Set percent for showing on bar
-		setPercent(per)
-
-		if (time > 30) {
-			setSendSeenAt(true)
-		}
-	}
-
-	/*
-	 * Function for scrolling to next story */
-	const scrollToNextStory = () => {
-		clearInterval(t)
-
-		// Get index of current story in array
-		index = props.stories.indexOf(props.story)
-		// Increment to get the next story if there are still some
-		if (index < props.stories.length - 1) {
-			index++
-		}
-		// Get ID of the story
-		var nextIndex = props.stories[index].id
-		// Get the Element of the story
-		var storyEl = document.getElementById(nextIndex)
-		// Get left offset of the element inorder to know how far to scroll
-		var left = storyEl.offsetLeft
-		// Scroll to next story element
-		props.storyScroller.current.scrollTo({
-			top: 0,
-			left: left,
-			behavior: "smooth",
-		})
-	}
+	const [handleTimer, setHandleTimer] = useState()
 
 	/*
 	 * Mark story as seen */
@@ -159,6 +48,16 @@ const Story = (props) => {
 		}
 	}
 
+	/*
+	 * Delete story */
+	const onDelete = () => {
+		axios
+			.delete(`/api/stories/${props.story.id}`)
+			.then((res) => props.setMessages([res.data.message]))
+			.catch((err) => props.getErrors(err))
+	}
+
+	console.log("render")
 	return (
 		<span id={props.story.id} className="single-story">
 			<div style={{ height: "100vh", width: "auto" }} onClick={onFreeze}>
@@ -184,19 +83,16 @@ const Story = (props) => {
 
 			{/* Floating Video Info Top */}
 			<div style={{ position: "absolute", top: 0, left: 0, width: "100%" }}>
-				<div className="d-flex">
-					{/* Track */}
-					{props.story.media.map((story, key) => (
-						<div key={key} className="w-100 pt-2 mx-2">
-							<div className="progress rounded-0" style={{ height: "2px" }}>
-								<div
-									className="progress-bar bg-warning"
-									style={{ width: segment == key ? percent : 0 }}></div>
-							</div>
-						</div>
-					))}
-					{/* Track End */}
-				</div>
+				<StoryProgressBar
+					story={props.story}
+					stories={props.stories}
+					storyScroller={props.storyScroller}
+					setSendSeenAt={setSendSeenAt}
+					setTimer={setTimer}
+					segment={segment}
+					setSegment={setSegment}
+					setHandleTimer={setHandleTimer}
+				/>
 				<div className="d-flex justify-content-between">
 					{/* Close Icon */}
 					<div className="">
@@ -264,6 +160,14 @@ const Story = (props) => {
 									onClick={onMute}>
 									<h6>{hasMuted ? "Unmute" : "Mute"}</h6>
 								</a>
+								{props.story.username == props.auth?.username && (
+									<a
+										href="#"
+										className="dropdown-item border-bottom border-dark"
+										onClick={onDelete}>
+										<h6>Delete</h6>
+									</a>
+								)}
 							</div>
 						</div>
 					</div>
